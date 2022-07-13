32c727d3ab516383a9e3cf9c546fb80f469378e9
Christian Guizar <guizar_chris@yahoo.com>
2022-07-13 10:22:02 -0500

    Daily Codewars Push conditional and mod

--- js/main.js
+++ js/main.js
@@ -1,25 +1,13 @@
-// The Story:
-// Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers.
+// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
 
-// Task Overview:
-// You have to write a function that accepts three parameters:
+// Examples:
+// 1) n =   3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
+// 2) n =  12, x = 2, y = 6 =>  true because  12 is divisible by 2 and 6
+// 3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
+// 4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
 
-// cap is the amount of people the bus can hold excluding the driver.
-// on is the number of people on the bus excluding the driver.
-// wait is the number of people waiting to get on to the bus excluding the driver.
-// If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
-
-// Usage Examples:
-// cap = 10, on = 5, wait = 5 --> 0 # He can fit all 5 passengers
-// cap = 100, on = 60, wait = 50 --> 10 # He can't fit 10 of the 50 waiting
-
-// function enough(cap, on, wait) {
-//   if(on + wait < cap){
-//     return 0
-//   }
-//   else{
-//     return on + wait - cap
-//   }
-//   // your code here
+// function isDivisible(n, x, y) {
+//   return n % x === 0 && n % y === 0
 // }
-const enough = (cap, on, wait) => on + wait < cap ? 0 : on + wait - cap
+
+const isDivisible = (n, x, y) => n % x == 0 && n % y == 0
