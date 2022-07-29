79d120a53174134230679b03249c0580b05d9456
Christian Guizar <guizar_chris@yahoo.com>
2022-07-29 10:58:48 -0500

    Daily Codewars Push Arrays and Methods

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
-// Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
-// Can you help her?
+// Create a function with two arguments that will return an array of the first (n) multiples of (x).
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+// Return the results as an array (or list in Python, Haskell or Elixir).
+// Examples:
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
 
-// function greet(name){
-//   if(name === "Johnny")
-//     return "Hello, my love!";
-//   else{
-//     return "Hello, " + name + "!";
+function countBy(x, n) {
+  var z = [];
+  for (let i = 1; i <= n; i++) {
+      z.push(x * i);
+  }
+  return z;
+}
+
+// const countBy = (x, n) => Array.from({length: n}, (v, k) => (k + 1) * x)
+
+// function countBy(x, n) {
+//   var z = [];
+//   for (var i=x; i<=(x*n); i+=x){
+//   z.push(i);
 //   }
-// }
+//   return z
+//   }
 
-const greet = name => name === "Johnny" ? "Hello, my love!" : "Hello, " + name + "!";
-
-// function greet(name){
-//   return "Hello, " + (name == "Johnny" ? "my love" : name) + "!";
-// }
+// const countBy = (x, n) =>
+//   [...Array(n)].map((_, idx) => ++idx * x);
