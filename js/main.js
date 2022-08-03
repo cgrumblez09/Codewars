1fe4859eb06dcadfe72c38498c46ab960f2c31cf
Christian Guizar <guizar_chris@yahoo.com>
2022-08-03 11:00:52 -0500

    Daily Codewarspush Arrays

--- js/main.js
+++ js/main.js
@@ -1,15 +1,34 @@
-// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+// For every good kata idea there seem to be quite a few bad ones!
 
-// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+// In this kata you need to check the provided array (x) for good ideas 'good' and 
+// bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', 
+// if there are more than 2 return 'I smell a series!'. If there are no good ideas, 
+// as is often the case, return 'Fail!'.
 
-// Example:
-// n= 5, m=5: 25
-// n=-5, m=5:  0
-// Waiting for translations and Feedback! Thanks!
-
-function paperwork(n, m) {
-  return n < 0 || m < 0 ? 0 : n * m
+function well(x) {
+  let res = []
+  res = x.filter(el => el == 'good')
+    if (res.length > 2) {
+    return 'I smell a series!'
+  } else if (res.length > 0 && res.length <= 2) {
+    return 'Publish!'
+  } else if (res.length < 1) {
+    return 'Fail!'
+  } 
 }
 
+// const well = x => {
+//   const good_count = x.filter(x => x == 'good').length;
+//   return good_count < 1 ? 'Fail!' : 
+//          good_count < 3 ? 'Publish!' : 'I smell a series!';
+// }
 
-// const paperwork = (n, m) => Math.max(0, n) * Math.max(0, m);
+// function well(x) {
+//   const count = x.reduce((s, v) => s + (v == 'good'), 0);
+//   return count ? count > 2 ? 'I smell a series!' : 'Publish!' : 'Fail!';
+// }
+
+// const  well = x => {
+//   let count = x.filter(el => el == 'good').length
+//   return  count > 2 ? 'I smell a series!' : count > 0 ? 'Publish!' : 'Fail!'; 
+// }
