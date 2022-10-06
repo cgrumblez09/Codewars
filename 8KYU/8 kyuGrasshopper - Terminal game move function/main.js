077a5ac2e6116616157b80ac8a7db5379e70940b
Christian Guizar <guizar_chris@yahoo.com>
2022-10-06 12:13:32 -0500

    Daily CW Push 8Kyu and very hard 7Kyu

---
+++ 8KYU/8 kyuGrasshopper - Terminal game move function/main.js
@@ -0,0 +1,22 @@
+// Terminal game move function
+// In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+// Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+// Example:
+// move(3, 6) should equal 15
+
+// function move (position, roll) {
+//     // return the new position
+//   }
+
+// PREP
+// A simple kata. The position parameter is where the player starts the round.
+// The roll parameter will double (*2) whatever the value is
+// The position will add to the doubled roll parameter for a final return
+
+function move (position, roll) {
+    return position + roll * 2
+  }
+
+//   const move = (position, roll) => position + roll * 2
