0475bacc487be95aabc419d8896611113e72dbe3
Christian Guizar <guizar_chris@yahoo.com>
2022-11-29 22:01:16 -0600

    Daily CW Push Fundamentals

---
+++ 7KYU/7 kyuHow many arguments/main.js
@@ -0,0 +1,18 @@
+// Create a function called args_count that returns the number of arguments provided
+
+// args_count(1, 2, 3) -> 3
+// args_count(1, 2, 3, 10) -> 4
+
+function args_count(){
+    return [...arguments].length
+  }
+
+//   function args_count() {
+//     return arguments.length;
+//   }
+
+//   const args_count = (...args) => args.length
+
+//   function args_count(...args){
+//     return args.length
+//   }
